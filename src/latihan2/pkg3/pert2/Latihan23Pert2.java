df20a0468c51cbec05e0b641fffe05ba29134a3d
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-25 06:24:00 +0700

    Initial commit

---
+++ src/latihan2/pkg3/pert2/Latihan23Pert2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2.pkg3.pert2;
+
+/**
+ *
+ * @author USER99
+ */
+public class Latihan23Pert2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bilangan;
+        
+        for (bilangan = 10 ; bilangan >=1; bilangan-- ) {
+            System.out.println("bilangan ke=" +bilangan);
+        
+        bilangan = 1;    
+        while (bilangan <=10) {
+            System.out.println("Bilangan ke=" +bilangan);
+            bilangan++;
+        
+        bilangan = 1;
+        do {
+            System.out.println("Bilangan ke=" +bilangan);
+            bilangan++;
+        } while (bilangan <=10);
+        
+        }
+        
+        }
+        
+    }
+    
+}
+
